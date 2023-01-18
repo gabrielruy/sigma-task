4a9f30d754687486dc5753d782a2f95ca111c524
gabrielruy <happyarcsestra@gmail.com>
2023-01-18 16:35:14 -0300

    Git init

---
+++ src/main/java/com/task/sigma/SigmaExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.task.sigma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigmaExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigmaExerciseApplication.class, args);
+	}
+
+}
